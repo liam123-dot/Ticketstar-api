2b3ce64a77a6503a131acbc2bf0fd220766930f0
Liam Buchanan <admin@ticketstar.uk>
2023-07-14 12:23:15 +0100

    Modifying some things

---
+++ v2/Ticketstar-API/layers/Exceptions/python/LambdaInvokationExceptions.py
@@ -0,0 +1,9 @@
+
+class InvalidBodyException(Exception):
+    def __init__(self, message, key):
+        self.message = message
+        self.key = key
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message} - {self.key}'
