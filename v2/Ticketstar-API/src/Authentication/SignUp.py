f9700d53d566a0a5ae02c330a5d46e5071735d77
Liam Buchanan <admin@ticketstar.uk>
2023-07-14 17:07:40 +0100

    Sign Up function finished

--- v2/Ticketstar-API/src/Authentication/SignUp.py
+++ v2/Ticketstar-API/src/Authentication/SignUp.py
@@ -7,8 +7,6 @@ This contains many aspect of error handling and verification.
 We need many functions to fulfill this.
 """
 
-
-
 """
 SignUpFunction:
 Comes from the sign up screen.
@@ -18,16 +16,7 @@ first_name, last_name, email, phone_number, password
 
 uses the cognito sdk to sign up.
 
-Possible Exceptions that need to be handled due to user error only:
-UsernameExistsException, InvalidPasswordException - message returned to user
 
-Error that could be due to the user or Ticketstar:
-CodeDeliveryFailureException, error with sending the verification code
-
-
-Error that is due to Ticketstar:
-InvalidEmailRoleAccessPolicyException
-InvalidSmsRoleAccessPolicyException
 
 Response from cognito:
 
@@ -43,4 +32,127 @@ Response from cognito:
 
 """
 
-def lambda_handler(event, context)
+from LambdaInvokationExceptions import InvalidBodyException
+from botocore.exceptions import ClientError
+from utils import get_secret_hash, SecretHashGenerationException
+import boto3
+import json
+import os
+import logging
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Initialize Cognito client
+cognito = boto3.client('cognito-idp')
+
+# Cognito configuration
+USER_POOL_ID = os.environ.get('USER_POOL_ID')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+
+
+def lambda_handler(event, context):
+    logger.info('Processing event: %s', event)  # log the incoming event
+    try:
+        # Load the attributes and check whether all the required inputs are sent
+        body = json.loads(event['body'])
+        email = body['email'].lower()
+        password = body['password']
+        first_name = body['firstName']
+        last_name = body['lastName']
+        phone_number = body['phoneNumber']
+    except (InvalidBodyException, json.JSONDecodeError) as e:
+        logger.error('Error processing event: %s', e)  # log the error
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': str(e),
+                'reason': 'InvalidBody' if isinstance(e, InvalidBodyException) else 'InvalidJson'
+            })
+        }
+
+    try:
+        confirmation_method = submit_to_cognito(email, password, first_name, last_name, phone_number)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'message': 'User successfully registered',
+                'confirmation_method': confirmation_method
+            })
+        }
+    except SecretHashGenerationException:
+        logger.error('Error generating secret hash')  # log the error
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': 'Error generating secret hash',
+                'reason': 'secret_hash'
+            })
+        }
+    except ClientError as e:
+        error = e.response['Error']
+        logger.error('Client error: %s', error)  # log the error
+        if error['Code'] in ['UsernameExistsException', 'InvalidPasswordException']:
+            return {
+                'statusCode': 401,
+                'body': json.dumps({
+                    'message': error['Message'],
+                    'reason': error['Code']
+                })
+            }
+        elif error['Code'] == 'CodeDeliveryFailureException':
+            return {
+                'statusCode': 401,
+                'body': json.dumps({
+                    'message': error['Message'],
+                    'reason': 'CodeDeliveryFailure'
+                })
+            }
+        elif error['Code'] in ['InvalidEmailRoleAccessPolicyException', 'InvalidSmsRoleAccessPolicyException']:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    'message': error['Message'],
+                    'reason': error['Code']
+                })
+            }
+        else:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({
+                    'message': 'Unexpected error occurred',
+                    'reason': 'unknown_error'
+                })
+            }
+
+
+def submit_to_cognito(email, password, first_name, last_name, phone_number):
+
+    secret_hash = get_secret_hash(username=email, CLIENT_ID=CLIENT_ID, CLIENT_SECRET=CLIENT_SECRET)
+
+    attributes = [
+        {'Name': 'email', 'Value': email},
+        {'Name': 'given_name', 'Value': first_name},
+        {'Name': 'family_name', 'Value': last_name},
+        {'Name': 'phone_number', 'Value': phone_number}
+    ]
+
+    response = cognito.sign_up(
+        ClientId=CLIENT_ID,
+        SecretHash=secret_hash,
+        Username=email,
+        Password=password,
+        UserAttributes=attributes
+    )
+
+    confirmation_method = response['CodeDeliveryDetails']['AttributeName']
+
+    return confirmation_method
+
+
+
+
+
--- v2/Ticketstar-API/src/Authentication/utils.py
+++ v2/Ticketstar-API/src/Authentication/utils.py
@@ -3,8 +3,20 @@ import hashlib
 import base64
 
 
+class SecretHashGenerationException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'Error generating secret hash: {self.message}'
+
+
 def get_secret_hash(username, CLIENT_ID, CLIENT_SECRET):
-    msg = username + CLIENT_ID
-    dig = hmac.new(str(CLIENT_SECRET).encode('utf-8'),
-                   msg=str(msg).encode('utf-8'), digestmod=hashlib.sha256).digest()
-    return base64.b64encode(dig).decode()
+    try:
+        msg = username + CLIENT_ID
+        dig = hmac.new(str(CLIENT_SECRET).encode('utf-8'),
+                       msg=str(msg).encode('utf-8'), digestmod=hashlib.sha256).digest()
+        return base64.b64encode(dig).decode()
+    except Exception as e:
+        raise SecretHashGenerationException(str(e))
